97128aa95faa39f90377a66513e8f21f5bd82ee7
Vishalakshiii <91335213+Vishalakshiii@users.noreply.github.com>
2022-11-10 13:18:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,10 @@
-# Caching the mean of the vector
 makeVector <- function(x = numeric()) {
         m <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
-        }
+                m <<- NULL }
         get <- function() x
         setmean <- function(mean) m <<- mean
         getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+        list(set = set, get = get,setmean = setmean,getmean = getmean)
 }
